3ecf51267f4bfa87ddd7cdd8c078e3c79d3f38a5
Tyler Zentz <tylerzentz@Tylers-MacBook-Pro-2.local>
2015-12-05 18:25:09 -0500

    isValidObject function added to users.js

--- federation/routes/users.js
+++ federation/routes/users.js
@@ -7,11 +7,46 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+function isValidObject(user){
+	var result = {};
+	var username = user.username;
+	var password = user.password;
+	console.log(username + " " + password);
+	if(username && username.length > 0){
+		result.success = true;
+		result.desc = "";
+	}
+	else{
+		result.success = false;
+		result.desc = "username not valid";
+	}
+
+	if(result.success == true){
+
+	if(password && password.length > 0){
+		result.success = true;
+		result.desc = "";
+	}
+	else{
+		result.success = false;
+		result.desc = "password not valid";
+	}
+}
+	return result;	
+}
+
 router.post('/register', function(req, res, next) {
 	var user = req.body;
+	console.log(req.body);
+	console.log(user);
+	if(!isValidObject(user).success){
+		res.json(isValidObject(user));
+	}
+	else{
 	userController.registerNewUser(user, function(result){
 		res.json(result);
 	});
+}
 });
 
 router.post('/editpassword', function(req, res, next) {
@@ -20,28 +55,28 @@ router.post('/editpassword', function(req, res, next) {
 		res.json(result);
 	});
 });
-
+//returns a password hash, logs in the user
 router.post('/login', function(req, res, next) {
 	var user = req.body;
 	userController.loginUser(user, function(result){
 		res.json(result);
 	});
 });
-
+//returns the id of the search in the table, adds a new saved search string
 router.post('/addsearch', function(req, res, next) {
 	var user = req.body;
 	userController.addSearch(user, function(result){
 		res.json(result);
 	});
 });
-
+//removes a saved search by id
 router.post('/removesearch', function(req, res, next) {
 	var user = req.body;
 	userController.removeSearch(user, function(result){
 		res.json(result);
 	});
 });
-
+//returns a list of all user's saved searches as [{"id": 12, "search" : ""}, ]
 router.get('/getsearches', function(req, res, next) {
 	var user = req.body;
 	userController.getSearches(user, function(result){
