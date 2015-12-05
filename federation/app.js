ef29787985590898b5b8d882567f738a1c838379
Khalory <tk100794@gmail.com>
2015-12-05 20:52:18 +0000

    Initial routing, created controller folder for federation

--- federation/app.js
+++ federation/app.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routes);
+app.use('/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
