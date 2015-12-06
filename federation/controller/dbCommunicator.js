177e26efb5e01155fd9aef9d8d3e1554d007e444
Khalory <tk100794@gmail.com>
2015-12-06 21:22:02 +0000

    Federation: Added dbCommunicator as a new controller

---
+++ federation/controller/dbCommunicator.js
@@ -0,0 +1,34 @@
+var sqlite3 = import('sqlite3').verbose();
+//TODO: Handle separate databases
+//TODO: Get actually database locations
+var db = sqlite3.Database('../database/db', sqlite3.OPEN_READWRITE);
+
+//callback: function(error)
+//	this.lastID is the last inserted row ID
+//	this.changes is the # of rows affected
+//
+exports.run = function(location, queryString, callback) {
+	db.run(queryString, [], callback);
+}
+
+//callback: function(error, row)
+//	This is executed only for the first row returned
+exports.get = function(location, queryString, callback) {
+	db.get(queryString, [], callback);
+}
+
+//callback: function(error, rows)
+//	This is executed on a list of rows
+exports.all = function(location, queryString, callback) {
+	db.all(queryString, [], callback);
+}
+
+//callback: function(error, row)
+//	This is executed on each row returned separately
+//complete: function(error, #rows)
+//	This is called once all row callbacks have been called
+//While all() requires the results to be in memory, this
+//is much more efficient for large queries
+exports.each = function(location, queryString, callback, complete) {
+	db.each(queryString, [], callback, complete)
+}
